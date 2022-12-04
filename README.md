44a3ca2d5891fdfb0a53eba866015031acedfc08
Akshay Kumar <akshay.kumar@smallcase.com>
2022-12-04 09:59:52 +0530

    Add logic to mint nft

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flyover - eth india 2022 project
+
+
+Polygon scan link: https://mumbai.polygonscan.com/address/0xC6D747bDCbB7A41Df957249CD14901b9786010cB
--- index.js
+++ index.js
@@ -22,12 +22,12 @@ const common = Common.custom(CustomChain.PolygonMumbai);
 //addresses
 const contractAddress = process.env.CONTRACT_ADDRESS;
 console.log(process.env.CONTRACT_ADDRESS);
-const adminAddress = "0xe2fF75F5D264a68D1E561A27b4Cdb886f5C8ac50";
+const adminAddress = "0x41d1B1E95a195bAd248D5a7C04A9b8F8c455AD75";
 const user1Address = "0xe196C91ABFb4DFba4c57704C530Be52C3c3ddD9B";
 const user2Address = "0xB63Cf430fe1Ca8d80dff1F714B71fD688e8F5F6d";
 
 const adminPrivateAddress = Buffer.from(
-  "78481e093cfec33328a6d9399ba63147527b7b5c8d71071f96647781fb464e9d",
+  "1ca6d84104fe673b5d9be8ec5f97078fd732043745f5927a437a2d03f219f08d",
   "hex"
 );
 
@@ -41,37 +41,41 @@ var cors = require("cors");
 
 app.use(cors());
 
-app.get("/get-user-data", async (req, res) => {
-    const id = req.query.id;
-    const user = users.users.find((user) => parseInt(id) === user.id);
-    const userId = user.publicKey;
-    const nftCount = await contract.methods.totalSupply().call();
-    const userNfts = [];
-    for(let i = 0; i <= parseInt(nftCount); i++) {
-        const ownerAddress = await contract.methods.nftToOwner(i).call();
-        if(ownerAddress && String(ownerAddress) === String(userId)) {
-            const {name, dna, level, readyTime, winCount, lossCount } = await contract.methods.nfts(i).call();
-            userNfts.push({ name, dna, level, readyTime, winCount, lossCount });
-        }
-    }
-    return res.json(200, { userId, userNfts });
-});
+// app.get("/get-user-data", async (req, res) => {
+//     const id = req.query.id;
+//     const user = users.users.find((user) => parseInt(id) === user.id);
+//     const userId = user.publicKey;
+//     const nftCount = await contract.methods.totalSupply().call();
+//     const userNfts = [];
+//     for(let i = 0; i <= parseInt(nftCount); i++) {
+//         const ownerAddress = await contract.methods.nftToOwner(i).call();
+//         if(ownerAddress && String(ownerAddress) === String(userId)) {
+//             const {name, dna, level, readyTime, winCount, lossCount } = await contract.methods.nfts(i).call();
+//             userNfts.push({ name, dna, level, readyTime, winCount, lossCount });
+//         }
+//     }
+//     return res.json(200, { userId, userNfts });
+// });
 
 const sendTransaction = (transaction) =>
   new Promise((res, rej) => {
     // console.log(transaction.toString());
-    web3js.eth
-      .sendSignedTransaction("0x" + transaction.serialize().toString("hex"))
-      .on("transactionHash", (hash) => {
-        res(hash);
-        // res.status(200).json({ message: "Transaction successful", transactionId: hash })
-      });
+    try {
+
+        web3js.eth
+        .sendSignedTransaction("0x" + transaction.serialize().toString("hex"))
+        .on("transactionHash", (hash) => {
+            res(hash);
+            // res.status(200).json({ message: "Transaction successful", transactionId: hash })
+        });
+    } catch(error) {
+        console.log(error);   
+    }
   });
 
 app.get("/mint-tokens", async (req, res) => {
-  const { level } = req.query;
-  const userInLevel = users.users.find((user) => user.level === parseInt(level));
-  if(userInLevel) {
+  try {
+  const { receiver, name } = req.query;
 
     const count = await getTransactionCount(adminAddress);
     console.log(count);
@@ -82,10 +86,10 @@ app.get("/mint-tokens", async (req, res) => {
         from: adminAddress,
         to: contractAddress,
         value: "0x0",
-        gasPrice: web3js.utils.toHex(30 * 1e9),
-        gasLimit: web3js.utils.toHex(310000),
+        gasPrice: web3js.utils.toHex(40 * 1e9),
+        gasLimit: web3js.utils.toHex(410000),
         data: contract.methods
-        .mintNft("0xe196C91ABFb4DFba4c57704C530Be52C3c3ddD9B", "new1", 1234, "new1")
+        .mintNft(receiver, name, 1234, name)
         .encodeABI(),
         nonce: web3js.utils.toHex(count),
     };
@@ -100,7 +104,12 @@ app.get("/mint-tokens", async (req, res) => {
     res
         .status(200)
         .json({ message: "Transaction successful", transactionId: hash });
-  }
+    } catch(error) {
+        console.log(error)
+        res.json(400, {
+            message: "transaction unsuccessful"
+        })
+    }
 });
 
 app.listen(3030, () => console.log("Example app listening on port 3030!"));
